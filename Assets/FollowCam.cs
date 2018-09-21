c236d31c8c6dbb40a21c6bad801a8e7a72b94292
Luthere15 <42876725+Luthere15@users.noreply.github.com>
2018-09-21 18:02:25 -0400

    pg 512

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI;
+
+    [Header("Set Dynamically")]
+    public float camZ;
+
+    private void Awake()
+    {
+        camZ = this.transform.position.z;
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (POI == null) return;
+
+        Vector3 destination = POI.transform.position;
+
+        destination.z = camZ;
+
+        transform.position = destination;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
